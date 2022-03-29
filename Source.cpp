49ecb0a7fed1f310f08308910d5161add19da571
hvdhavietduc <80439464+hvdhavietduc@users.noreply.github.com>
2022-03-29 17:47:13 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -32,54 +32,39 @@ int partition(int *arr,int low, int high)
 	return (i + 1);
 }
 int main() {
-	ifstream file("Text.txt");
+	
 	fstream output1;
 	fstream output2;
 	output1.open("output1.txt", ios::out | ios::in);
-	output2.open("output2.txt", ios::out | ios::in);
+	
 	int n = 500000;
 	int* arr = new int[n];
-	int i = 0;
 	string tmp = "";
-	bool check_next_file = false;
-	while ( !file.eof()) {
-		getline(file, tmp);
-		arr[i] = stoi(tmp);
-		i++;
-		if (i == n) {
-			quickSort(arr, 0, i - 1);
-			int j = 0;
-			if (!check_next_file) {
-				while (i != j) {
-					if(j==n-1){
-						output1 << to_string(arr[j])<<endl << "END";
-					}
-					else output1 << to_string(arr[j]) << endl;
-					j++;
-				}
-				i = 0;
-				check_next_file = true;
-			}
-			else {
-				while (i != j) {
-					if (j == n-1) {
-						output2 << to_string(arr[j]) <<endl<< "END";
-					}
-					else output2 << to_string(arr[j]) << endl;
-					j++;
-				}
-				i = 0;
-			}
-		}
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	quickSort(arr, 0, n - 1);
+	for (int i = 0; i < n; i++) {
+		if (i == n - 1) output1 << to_string(arr[i]) << endl << "END";
+		else output1 << to_string(arr[i]) << endl;
+	}
+	
+	output1.close();
+	output2.open("output2.txt", ios::out | ios::in);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	quickSort(arr, 0, n - 1);
+	for (int i = 0; i < n; i++) {
+		if (i == n - 1) output2 << to_string(arr[i]) << endl << "END";
+		else output2 << to_string(arr[i]) << endl;
 	}
 	delete[] arr;
-	file.close();
-	output1.close();
 	output2.close();
 	output1.open("output1.txt", ios::in);
 	output2.open("output2.txt", ios::in);
 	fstream finalOutput;
-	finalOutput.open("finalOutput.txt", ios::out);
+	finalOutput.open("finalOutput.txt", ios::in |ios::out);
 	string tmp1, tmp2;
 	getline(output1, tmp1);
 	getline(output2, tmp2);
@@ -118,6 +103,13 @@ int main() {
 				integer2 = stoi(tmp2);
 			}
 		}
+	}
+	finalOutput.close();
+	finalOutput.open("finalOutput.txt", ios::in);
+	cout << "Sort 1 milion integer :"<<endl;
+	while (!finalOutput.eof()) {
+		 getline(finalOutput, tmp);
+		 cout << tmp << " ";
 	}
 	finalOutput.close();
 	output1.close();
